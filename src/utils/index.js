e908d88d2b6ee1896b1d94172b23af23cb7a3ca0
BostonWafro <wafrogaming@gmail.com>
2021-02-08 11:57:01 +0000

    added all functions to mirror database in utils folder, will now set up locations in app to interact and test

--- src/utils/index.js
+++ src/utils/index.js
@@ -10,7 +10,59 @@ const login = async (event, email, password, setUser) => {
     });
     const data = await response.json();
     setUser(data)
-}
+};
+
+const logout = async (event, user) => {
+    event.preventDefault();
+    const response = await fetch('http://localhost/5000/users/logout', {
+        method: 'GET',
+        headers: { 'Authorization': `Bearer ${user.user.tokens[0].token}` },
+    });
+    const data = await response.json();
+    console.log(data);
+};
+
+const addUser = async (event, name, email, password, setUser) => {
+    event.preventDefault();
+    const response = await fetch('http://localhost/5000/users', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+            password: password,
+        }),
+    });
+    const data = await response.json();
+    setUser(data);
+};
+
+const updateUser = async (event, user, name, email, password, setUser) => {
+    event.preventDefault();
+    const response = await fetch(`http://localhost/5000/users/${user.user._id}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json',
+                   'Authorization': `Bearer ${user.user.tokens[0].token}` },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+            password: password,
+        }),
+    });
+    const data = await response.json();
+    setUser(data);
+};
+
+const deleteUser = async (event, user, setUser) => {
+    event.preventDefault();
+    const response = await fetch(`http://localhost/5000/users/${user.user._id}`, {
+        method: 'DELETE',
+        headers: { 'Authorization': `Bearer ${user.user.tokens[0].token}` },
+    });
+    const data = await response.json();
+    console.log(data);
+    setUser('');
+};
 
 const addPost = async (event, title, content, user) => {
     event.preventDefault();
@@ -25,7 +77,32 @@ const addPost = async (event, title, content, user) => {
     });
     const data = await response.json()
     console.log(data);
-}
+};
+
+const updatePost = async (event, title, content, user, post) => {
+    event.preventDefault();
+    const response = await fetch(`http://localhost:5000/posts/${post._id}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json',
+                   'Authorization': `Bearer ${user.user.tokens[0].token}` },
+        body: JSON.stringify({
+            title: title,
+            content: content,
+        }),
+    });
+    const data = await response.json();
+    console.log(data);
+};
+
+const deletePost = async (event, user, post) => {
+    event.preventDefault();
+    const response = await fetch(`http://localhost:5000/posts/${post._id}`, {
+        method: 'DELETE',
+        headers: { 'Authorization': `Bearer ${user.user.tokens[0].token}` },
+    });
+    const data = response.json();
+    console.log(data);
+};
 
 const fetchPosts = async (setPosts) => {
     const response = await fetch('http://localhost:5000/posts');
@@ -37,12 +114,18 @@ const fetchPosts = async (setPosts) => {
 const fetchUsers = async (setUsers) => {
     const response = await fetch('http://localhost:5000/users');
     const data = await response.json();
-    setUsers({data})
-}
+    setUsers(data);
+};
 
 export {
     login,
+    logout,
+    addUser,
+    updateUser,
+    deleteUser,
     addPost,
+    updatePost,
+    deletePost,
     fetchPosts,
     fetchUsers
-}
+};
